3e6243c8813e9906108350123b51edfff0793210
Ryusei <hmj2422001@stu.o-hara.ac.jp>
2025-08-28 16:21:01 +0900

    Revert "炎氷属性の切り替えをした"
    This reverts commit cbc6ecadbdeffd3ab886b12f6e03ed980f4c366b.

--- Assets/Enemy/Script/BossScript.cs
+++ Assets/Enemy/Script/BossScript.cs
@@ -1,53 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class BossScript : MonoBehaviour
 {
-	float modelTime = 5f;
 
 	float m_spped = 2;
-	bool m_transform;
 	Transform m_player;
 	Animator m_animator;
-
-	int m_modelIndex;
-
-	[SerializeField]
-	GameObject[] m_model;
-
-	enum ModelType
-	{
-		IceModel,
-		FireModel
-	}
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		m_model[(int)ModelType.IceModel].SetActive(true);
-		m_model[(int)ModelType.FireModel].SetActive(false);
 		m_player = GameObject.FindWithTag("Player").transform;
-		m_animator = m_model[(int)ModelType.IceModel].GetComponent<Animator>();
-		m_modelIndex = (int)ModelType.IceModel;
-		m_transform = true;
-		m_modelIndex = 0;
+		m_animator = GetComponent<Animator>();	
 	}
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-		modelTime -= Time.deltaTime;
-
-		if(modelTime <= 0)
-		{
-			m_animator.SetTrigger("Transform");
-			m_transform = true;
-			modelTime = 15;
-		}
-
-		if (m_transform) return;
 
 		// ƒvƒŒƒCƒ„[‚ðŒü‚­
         Vector3 forward = m_player.position - transform.position;
@@ -68,30 +39,4 @@ public class BossScript : MonoBehaviour
 
 		m_animator.SetBool("Move", isMove);
     }
-
-	public void Transform()
-	{
-		if(m_modelIndex == (int)ModelType.IceModel)
-		{
-			m_model[(int)ModelType.IceModel].SetActive(false);
-			m_model[(int)ModelType.FireModel].SetActive(true);
-
-			m_animator = m_model[(int)ModelType.FireModel].GetComponent<Animator>();
-			m_modelIndex = (int)ModelType.FireModel;
-		}
-		else
-		{
-			m_model[(int)ModelType.IceModel].SetActive(true);
-			m_model[(int)ModelType.FireModel].SetActive(false);
-			m_animator = m_model[(int)ModelType.IceModel].GetComponent<Animator>();
-			m_modelIndex = (int)ModelType.IceModel;
-		}
-		
-	}
-
-	public void TransformComplete()
-	{
-		m_transform = false;
-		Debug.Log("“ü‚Á‚½");
-	}
 }
